7b4305e538846edbadcf2fee91a26ed61afdb492
febriankusuma17 <febriankusuma01@gmail.com>
2018-11-08 08:39:43 +0700

    10

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -12,13 +12,13 @@
     <title>Login Interdaces SB admin1</title>
 
     <!-- Bootstrap core CSS-->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
 
     <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url('assets/fontawesome-free/css/all.min.css') ?>" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/sb-admin.css') ?>" rel="stylesheet">
 
   </head>
 
