4008a124bc5a7ac1e21cd4d3ea396a2aadf3fce1
febriankusuma17 <febriankusuma01@gmail.com>
2018-11-08 08:44:15 +0700

    10

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -6,10 +6,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="Login dengan akun anda" content="">
+    <meta name="febrian" content="">
 
-    <title>Login Interdaces SB admin1</title>
+    <title>Silahkan Login Dengan Username dan Password anda</title>
 
     <!-- Bootstrap core CSS-->
     <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
@@ -32,7 +32,7 @@
             <div class="form-group">
               <div class="form-label-group">
                 <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="required" autofocus="autofocus">
-                <label for="inputEmail">Email address</label>
+                <label for="inputEmail">Alamat Email</label>
               </div>
             </div>
             <div class="form-group">
@@ -45,26 +45,26 @@
               <div class="checkbox">
                 <label>
                   <input type="checkbox" value="remember-me">
-                  Remember Password
+                  Ingat Password
                 </label>
               </div>
             </div>
             <a class="btn btn-primary btn-block" href="index.html">Login</a>
           </form>
           <div class="text-center">
-            <a class="d-block small mt-3" href="register.html">Register an Account</a>
-            <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
+            <a class="d-block small mt-3" href="register.html">Buat akun</a>
+            <a class="d-block small" href="forgot-password.html">Lupa Password</a>
           </div>
         </div>
       </div>
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link href="<?php echo base_url('assets/jquery/jquery.min.js') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/bootstrap/js/bootstrap.bundle.min.js') ?>" rel="stylesheet">
 
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <link href="<?php echo base_url('assets/jquery-easing/jquery.easing.min.js') ?>" rel="stylesheet">
 
   </body>
 
