9105b3ac36d56cc0819e5b34d3ed9d010fdf6f28
febriankusuma17 <febriankusuma01@gmail.com>
2018-11-08 07:10:29 +0700

    ss

--- application/views/template.php
+++ application/views/template.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2</title>
+    <title>SB Admin 2 555555</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
